5b444fa1aeb156aff76a65de99c83b460f599d4d
Chris Hutchinson <chris@cshutchinson.com>
2015-03-25 19:31:35 -0400

    Added header guard to Game type.

--- include/trans4/game/Game.hpp
+++ include/trans4/game/Game.hpp
@@ -1,67 +1,74 @@
-#pragma once
+#ifndef RPGTOOLKIT_TRANS4_GAME_GAME_INCLUDED
+#define RPGTOOLKIT_TRANS4_GAME_GAME_INCLUDED
 
 #include <memory>
 #include <string>
 
 namespace clio {
-	class GameStateStack;
-	class Logger;
-	class System;
+
+    struct GameStateStack;
+    struct Logger;
+    struct System;
+
 }
 
 namespace rpgtoolkit {
 
-	/// \brief The game class is responsible for the game loop. 
-	///
-	/// The object contains a GameStateStack to manage the different
-	/// game states of the game. 
-	class Game {
-	public:
-		/// \brief The string corresponding to a Game log file
-		const static std::string LOG;
+    /// \brief The game class is responsible for the game loop.
+    ///
+    /// The object contains a GameStateStack to manage the different
+    /// game states of the game.
 
-		/// \brief Constructor
-		///
-		/// \param system The system with which to interact.
-		Game(clio::System* system);
+    struct Game {
 
-		/// \brief Destructor
-		~Game();
+        /// \brief The string corresponding to a Game log file
+        const static std::string LOG;
 
-		/// \brief Run the game loop until Quit() is called
-		void Run();
+        /// \brief Constructor
+        ///
+        /// \param system The system with which to interact.
+        Game(clio::System *system);
 
-		/// \brief Quit the game.
-		void Quit();
+        /// \brief Destructor
+        ~Game();
 
-		/// \brief Get the game stack.
-		clio::GameStateStack * const GetStateStack();
+        /// \brief Run the game loop until Quit() is called
+        void Run();
 
-		/// \brief No copying allowed.
-		Game(Game const&) = delete;
+        /// \brief Quit the game.
+        void Quit();
 
-		/// \brief No copying allowed.
-		Game & operator=(Game const&) = delete;
-	private:
-		/// \brief Logger for this class.
-		std::unique_ptr<clio::Logger> log_;
+        /// \brief Get the game stack.
+        clio::GameStateStack *const GetStateStack();
 
-		/// \brief Whether the game loop continues or not.
-		bool quit_;
+        /// \brief No copying allowed.
+        Game(Game const &) = delete;
 
-		/// \brief Manages the game states.
-		std::unique_ptr<clio::GameStateStack> stack_;
+        /// \brief No copying allowed.
+        Game & operator=(Game const &) = delete;
 
-		/// \brief The underlying system.
-		clio::System* system_;
+    private:
+        /// \brief Logger for this class.
+        std::unique_ptr<clio::Logger> log_;
 
-		/// \brief Helper function to poll input.
-		void Poll();
+        /// \brief Whether the game loop continues or not.
+        bool quit_;
 
-		/// \brief Helper function to update state(s).
-		void Update();
+        /// \brief Manages the game states.
+        std::unique_ptr<clio::GameStateStack> stack_;
 
-		/// \brief Helper function to render to the screen.
-		void Render();
-	};
+        /// \brief The underlying system.
+        clio::System *system_;
+
+        /// \brief Helper function to poll input.
+        void Poll();
+
+        /// \brief Helper function to update state(s).
+        void Update();
+
+        /// \brief Helper function to render to the screen.
+        void Render();
+    };
 }
+
+#endif
