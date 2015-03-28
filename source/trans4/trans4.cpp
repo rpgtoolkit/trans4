62dc1e5f9595069b4012106244e88216d6516425
mariobadr <mariobadr@gmail.com>
2015-03-28 18:05:59 -0400

    Output exceptions to dialog box

--- source/trans4/trans4.cpp
+++ source/trans4/trans4.cpp
@@ -3,14 +3,29 @@
 /// See LICENSE.md in the distribution for the full license text including,
 /// but not limited to, a notice of warranty and distribution rights.
 
+#include "clio/common/Exception.hpp"
+#include "clio/system/Dialog.hpp"
+
+//Have to include this after clio Exception
+//This is a hack anyway, should rethrow luabridge exceptions as tk ones
+#include "lua.hpp"
+#include "LuaBridge.h"
+
 #include "Engine.hpp"
 
 int main(int argc, char* argv[]) {
 
 	auto & engine = rpgtoolkit::Engine::GetInstance();
 
-	engine.Configure();
-	engine.Run();
+	try {
+		engine.Configure();
+		engine.Run();
+	} catch(clio::Exception e) {
+		clio::Dialog::ShowError("Clio Error", e.what());
+	}
+	catch (luabridge::LuaException e) {
+		clio::Dialog::ShowError("Lua Error", e.what());
+	}
 
 	return 0;
 
